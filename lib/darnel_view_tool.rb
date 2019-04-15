95e9b23dd255411602054bdab21358cb7c68a85e
doubleg <darnel_free@yahoo.com>
2019-04-15 01:29:43 -0500

    Updated gemspec and lib files

---
+++ lib/darnel_view_tool.rb
@@ -0,0 +1,6 @@
+require "darnel_view_tool/version"
+
+module DarnelViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/darnel_view_tool/version.rb
@@ -0,0 +1,3 @@
+module DarnelViewTool
+  VERSION = "0.1.0"
+end
